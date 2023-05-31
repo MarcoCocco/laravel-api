09a9db840c9bfaa435454eaab5cea86f5f946692
MarcoCocco <marcococco28@gmail.com>
2023-05-31 17:20:36 +0200

    feat: aggiunta la route api per lo slug aggiunta la funzione di request di filtraggio dei progetti per tipo

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,19 +4,59 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $projects = Project::with('type', 'technologies')
-        ->orderBy('projects.created_at', 'desc')
-        ->paginate(2);
+        $requestData = $request->all();
+        $types = Type::all();
 
-        return response()->json([
-            'success' => true,
-            'results' => $projects
-        ]);
+        if ($request->has('type_id') && $requestData['type_id']) {
+
+            $projects = Project::where('type_id', $requestData['type_id'])
+                ->with('type', 'technologies')
+                ->orderBy('projects.created_at', 'desc')
+                ->get();
+
+            if (count($projects) == 0) {
+                return response()->json([
+                    'success' => false,
+                    'error' => 'Nessun progetto fa parte di questa tipologia.',
+                ]);
+            }
+
+        } else {
+
+            $projects = Project::with('type', 'technologies')
+                ->orderBy('projects.created_at', 'desc')
+                ->get();
+
+            return response()->json([
+                'success' => true,
+                'results' => $projects,
+                'allTypes' => $types,
+            ]);
+
+        }
+    }
+
+    public function show($slug)
+    {
+        $project = Project::where('slug', $slug)->with('type', 'technologies')->first();
+
+        if ($project) {
+            return response()->json([
+                'success' => true,
+                'project' => $project,
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'error' => 'Il progetto non esiste.'
+            ]);
+        }
     }
 }
